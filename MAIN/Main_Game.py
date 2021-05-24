b682e385bc5dbc7abca7c032a4f861a09249e2cb
CaioGO <caio54777@gmail.com>
2021-05-24 17:12:26 -0300

    Menu com cara nova

--- MAIN/Main_Game.py
+++ MAIN/Main_Game.py
@@ -15,6 +15,7 @@ bg_img6 = pygame.image.load("Assets/Image/City8.jpg")
 bg_img7 = pygame.image.load("Assets/Image/City9.jpg")
 bg_img8 = pygame.image.load("Assets/Image/City.gif")
 bg_img9 = pygame.image.load("Assets/Image/City2_1.jpg")
+mn = pygame.image.load("Assets/Image/Nave.jpg")
 
 BG = pygame.transform.scale(bg_img, (1000, 500))
 BG2 = pygame.transform.scale(bg_img2, (1000, 500))
@@ -25,6 +26,7 @@ BG6 = pygame.transform.scale(bg_img6, (1000, 500))
 BG7 = pygame.transform.scale(bg_img7, (1000, 500))
 BG8 = pygame.transform.scale(bg_img8, (1000, 500))
 BG9 = pygame.transform.scale(bg_img9, (1000, 500))
+MN = pygame.transform.scale(mn, (1000, 500))
 
 
 CORRER = [pygame.image.load(os.path.join("Assets/Player", "p1_walk01.png")),
@@ -35,6 +37,7 @@ PULAR = pygame.image.load(os.path.join("Assets/Player", "p1_jump.png"))
 AGAIXAR = [pygame.image.load(os.path.join("Assets/Player", "p1_down7.png")), 
            pygame.image.load(os.path.join("Assets/Player", "p1_down8.png"))]
 
+DISP = pygame.image.load(os.path.join("Assets/Player", "p1_walk012.png"))
 
 class Alien:
     X_POS = 80
@@ -87,6 +90,7 @@ class Alien:
             self.alien_corre = True
             self.alien_pula = False
 
+    # Personagem agaixa
     def agaixa(self):
         self.image = self.agaixa_img[self.passo_index // 5]
         self.alien_rect = self.image.get_rect()
@@ -94,27 +98,29 @@ class Alien:
         self.alien_rect.y = self.Y_POS_AG
         self.passo_index += 1
 
+    # Personagem corre
     def corre(self):
         self.image = self.corre_img[self.passo_index // 5]
         self.alien_rect = self.image.get_rect()
         self.alien_rect.x = self.X_POS
         self.alien_rect.y = self.Y_POS
         self.passo_index += 1
-
+    
+    # Personagem pula
     def pula(self):
         self.image = self.pula_img
         if self.alien_pula:
-            self.alien_rect.y -= self.pulo_vel * 4
+            self.alien_rect.y -= self.pulo_vel * 5
             self.pulo_vel -= 0.8
         if self.pulo_vel < - self.vel_pulo:
             self.alien_pula = False
             self.pulo_vel = self.vel_pulo
 
+    # Personagem aparece
     def muda(self, win):
         win.blit(self.image, (self.alien_rect.x, self.alien_rect.y))
 
-# MAIN
-
+# Função principal do jojo
 def main():
     global velocidade, x_pos_bg, y_pos_bg, points, obstacles
     run = True
@@ -128,110 +134,135 @@ def main():
     death_count = 0
     player = Alien()
 
+    # Função para a pontuação
     def pontos():
         global points, velocidade
         points += 1
         if points % 100 == 0:
             velocidade += 1
 
+        # Faz a pontuação aparecer na tela
         text = font.render("Pontuação: " + str(points), True, (255, 255, 255))
         textRect = text.get_rect()
         textRect.center = (800, 40)
         win.blit(text, textRect)
 
+    # Função para o background
     def background():
 
         global x_pos_bg, y_pos_bg
 
-        if points <= 100:
+        # Faz com que o background mude conforme a pontuação aumenta
+        if points <= 1000:
+
             image_width = BG.get_width()
             win.blit(BG, (x_pos_bg, y_pos_bg))
             win.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width:
                 win.blit(BG, (image_width + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 200 and points > 100:
+        elif points <= 2000 and points > 1000:
 
             image_width2 = BG2.get_width()
             win.blit(BG2, (x_pos_bg, y_pos_bg))
             win.blit(BG2, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG2, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 300 and points > 200:
+        elif points <= 3000 and points > 2000:
 
             image_width2 = BG3.get_width()
             win.blit(BG3, (x_pos_bg, y_pos_bg))
             win.blit(BG3, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG3, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 400 and points > 300:
+        elif points <= 4000 and points > 3000:
 
             image_width2 = BG4.get_width()
+
             win.blit(BG4, (x_pos_bg, y_pos_bg))
             win.blit(BG4, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG4, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 500 and points > 400:
+        elif points <= 5000 and points > 4000:
 
             image_width2 = BG5.get_width()
             win.blit(BG5, (x_pos_bg, y_pos_bg))
             win.blit(BG5, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG5, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 600 and points > 500:
+        elif points <= 6000 and points > 5000:
 
             image_width2 = BG6.get_width()
             win.blit(BG6, (x_pos_bg, y_pos_bg))
             win.blit(BG6, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG6, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 700 and points > 600:
+        elif points <= 7000 and points > 6000:
 
             image_width2 = BG7.get_width()
             win.blit(BG7, (x_pos_bg, y_pos_bg))
             win.blit(BG7, (image_width2 + x_pos_bg, y_pos_bg))
+            
             if x_pos_bg <= -image_width2:
                 win.blit(BG7, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
-        elif points <= 800 and points > 700:
+        elif points <= 8000 and points > 7000:
 
             image_width2 = BG8.get_width()
             win.blit(BG8, (x_pos_bg, y_pos_bg))
             win.blit(BG8, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG8, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+                
             x_pos_bg -= velocidade
 
-        elif points > 800:
+        elif points > 8000:
 
             image_width2 = BG9.get_width()
             win.blit(BG9, (x_pos_bg, y_pos_bg))
             win.blit(BG9, (image_width2 + x_pos_bg, y_pos_bg))
+
             if x_pos_bg <= -image_width2:
                 win.blit(BG9, (image_width2 + x_pos_bg, y_pos_bg))
                 x_pos_bg = 0
+
             x_pos_bg -= velocidade
 
+    # Update do que irá aparecer na tela
     while run:
 
         for event in pygame.event.get():
@@ -239,8 +270,8 @@ def main():
                 run = False
 
         win.fill((255, 255, 255))
-        userInput = pygame.key.get_pressed()
 
+        userInput = pygame.key.get_pressed()
 
         background()
 
@@ -251,8 +282,11 @@ def main():
         pontos()
 
         clock.tick(30)
+
         pygame.display.update()
 
+
+# Ultima função, que apresentará o "menu" principal do jogo, e apresenta algumas regras
 def menu(perdeu):
 
     global points
@@ -260,33 +294,45 @@ def menu(perdeu):
     run = True
 
     while run:
-        win.fill((255, 255, 255))
-        image_width = BG.get_width()
-        image_height = BG.get_height()
-        win.blit(BG, (image_width, image_height))
-        font = pygame.font.SysFont(None, 30)
+        
+        # Faz o background do menu
 
+        win.fill((255, 255, 255))
+        image_width = MN.get_width()
+        image_height = MN.get_height()
+        win.blit(MN, (0, 0))
+
+        font = pygame.font.SysFont(None, 35)
+        font2 = pygame.font.SysFont(None, 50)
+ 
+        # Apresenta dicas e como iniciar o jogo
         if perdeu == 0:
-            text = font.render("Aperte as setas para começar", True, (0, 0, 0))
-            text2 = font.render("Somente as setas serão usadas durante o jogo", True, (0, 0, 0))
+            text = font2.render("Aperte as setas para começar", 50, (0, 255, 0))
+            text2 = font.render("Somente as setas serão usadas durante o jogo", True, (255, 233, 0))
+            text3 = font.render("Não encoste em nenhum obstáculo!!!", True, (255, 0, 80))
 
         elif perdeu > 0:
-            text = font.render("Aperte as setas para começar", True, (0, 0, 0))
-            text2 = font.render("Somente as setas serão usadas durante o jogo", True, (0, 0, 0))
+            text = font.render("Aperte as setas para começar", True, (0, 255, 0))
+            text2 = font.render("Somente as setas serão usadas durante o jogo", True, (255, 233, 0))
+            text3 = font.render("Não encoste em nenhum obstáculo!!!", True, (255, 0, 80))
             score = font.render("Sua Pontuação: " + str(points), True, (0, 0, 0))
             scoreRect = score.get_rect()
-            scoreRect.center = (500, 300)
+            scoreRect.center = (680, 370)
             win.blit(score, scoreRect)
 
         textRect = text.get_rect()
-        textRect.center = (490, 250)
+        textRect.center = (490, 130)
 
         textRect2 = text2.get_rect()
-        textRect2.center = (490, 300)
+        textRect2.center = (490, 435)
+
+        textRect3 = text3.get_rect()
+        textRect3.center = (490, 470)
 
         win.blit(text, textRect)
         win.blit(text2, textRect2)
-        win.blit(CORRER[0], (440, 110))
+        win.blit(text3, textRect3)
+        win.blit(DISP, (440, 200))
 
         pygame.display.update()
 
