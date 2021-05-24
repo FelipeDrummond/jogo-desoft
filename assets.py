aaccd51a104715bd7e7a6ec682e8eae8658415e6
Thomas Chabro <thomascc@al.insper.edu.br>
2021-05-24 11:10:28 -0300

    organização

---
+++ assets.py
@@ -0,0 +1,7 @@
+import pygame
+import os
+
+bg_img = pygame.image.load("Image/City5.jpg")
+bg = pygame.transform.scale(bg_img, (1000, 500))
+bg_img2 = pygame.image.load("Image/City2_1.jpg")
+bg2 = pygame.transform.scale(bg_img2, (1000, 500))
