82915cd58124939ca3333b443438d244fd4f5f72
CaioGO <caio54777@gmail.com>
2021-05-24 11:00:26 -0300

    Organizado

--- Background/BackgroundEvent1.py
+++ Background/BackgroundEvent1.py
@@ -3,9 +3,9 @@ import os
 
 pygame.init()
 win = pygame.display.set_mode((1000, 500))
-bg_img = pygame.image.load("Image/City5.jpg")
+bg_img = pygame.image.load("Assets/Image/City5.jpg")
 bg = pygame.transform.scale(bg_img, (1000, 500))
-bg_img2 = pygame.image.load("Image/City2_1.jpg")
+bg_img2 = pygame.image.load("Assets/Image/City2_1.jpg")
 bg2 = pygame.transform.scale(bg_img2, (1000, 500))
 
 width = 1000
